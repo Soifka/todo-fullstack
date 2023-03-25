a69cf47304045341016bdbd0f3df80def248d5a6
Julia Alekseeva <soifka@gmail.com>
2023-03-25 20:29:39 +0200

    Add toastify

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import TodoPage from './pages/ToDoPage/TodoPage';
 import history from './BrowserHistory';
 import { connect } from 'react-redux';
 import { authUserRequest } from './actions/actionCreator';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 
@@ -16,17 +18,48 @@ function App(props) {
     }
   }, [])
 
+  useEffect(() => {
+    if(props.notification) {
+      toast(props.notification.notification, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      })
+    }
+  }, [props.notification]);
+
+  console.log(props);
+
   return (
     <HistoryRouter history={history}>
+      <ToastContainer 
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tasks/" element={<TodoPage />} />
       </Routes>
     </HistoryRouter>
+
+    
   );
 }
 
-const mapStateToProps = ({ user }) => ({ user });
+const mapStateToProps = ({ user, notification }) => ({ user, notification });
 
 const mapDispatchToProps = {
   authUserRequest
